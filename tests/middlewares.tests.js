450d3e69d72a556180de29bf9df60c44922e20e5
Sandrino Di Mattia <contact@sandrino.be>
2016-10-14 14:02:54 +0200

    Test aud validation in source control tools

--- tests/middlewares.tests.js
+++ tests/middlewares.tests.js
@@ -47,14 +47,14 @@ describe('#middlewares', function() {
     it('should return "Authentication required for this endpoint" error', function(done) {
       const app = express();
 
-      app.use('/', middlewares.requireUser);
+      app.use('/', middlewares.requireUser('foo.auth0.com'));
 
       request(app)
         .get('/')
         .expect(401)
         .end(function(err, res) {
           if (err) throw err;
-          expect(res.error).toMatch(/UnauthorizedError: Authentication required for this endpoint/);
+          expect(res.text).toMatch(/UnauthorizedError: Authentication required for this endpoint/);
           done();
         });
     });
@@ -62,11 +62,11 @@ describe('#middlewares', function() {
     it('should work', function(done) {
       const app = express();
       const addUser = function(req, res, next) {
-        req.user = { name: 'User' };
+        req.user = { name: 'User', aud: [ 'https://foo.auth0.com/api/v2/' ] };
         next();
       };
 
-      app.use('/', addUser, middlewares.requireUser, function(req, res) {
+      app.use('/', addUser, middlewares.requireUser('foo.auth0.com'), function(req, res) {
         res.send();
       });
 
@@ -75,6 +75,27 @@ describe('#middlewares', function() {
         .expect(200)
         .end(function(err) {
           if (err) throw err;
+          done();
+        });
+    });
+
+    it('should validate the audience', function(done) {
+      const app = express();
+      const addUser = function(req, res, next) {
+        req.user = { name: 'User', aud: [ 'https://somethingelse.auth0.com/api/v2/' ] };
+        next();
+      };
+
+      app.use('/', addUser, middlewares.requireUser('foo.auth0.com'), function(req, res) {
+        res.send();
+      });
+
+      request(app)
+        .get('/')
+        .expect(401)
+        .end(function(err, res) {
+          if (err) throw err;
+          expect(res.text).toMatch(/Invalid token. Audience does not match: foo.auth0.com/);
           done();
         });
     });
