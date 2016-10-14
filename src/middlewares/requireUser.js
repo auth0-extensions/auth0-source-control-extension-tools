045a0366a1f6318f957f22ed7eadaa815f105a46
Sandrino Di Mattia <contact@sandrino.be>
2016-10-14 13:57:24 +0200

    Additional audience validation

--- src/middlewares/requireUser.js
+++ src/middlewares/requireUser.js
@@ -1,9 +1,15 @@
 const UnauthorizedError = require('auth0-extension-tools').UnauthorizedError;
 
-module.exports = function(req, res, next) {
-  if (!req.user) {
-    return next(new UnauthorizedError('Authentication required for this endpoint.'));
-  }
+module.exports = function(domain) {
+  return function(req, res, next) {
+    if (!req.user) {
+      return next(new UnauthorizedError('Authentication required for this endpoint.'));
+    }
 
-  return next();
+    if (!req.user.aud || req.user.aud.indexOf('https://' + domain + '/api/v2/') === -1) {
+      return next(new UnauthorizedError('Invalid token. Audience does not match: ' + domain));
+    }
+
+    return next();
+  };
 };
