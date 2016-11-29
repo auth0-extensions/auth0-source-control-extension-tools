bdced9c40772a9687c460fa2a530b23473dae209
Sandrino Di Mattia <contact@sandrino.be>
2016-11-29 09:47:47 +0100

    Remove obsolete middlewares

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-const middlewares = require('./middlewares');
 const constants = require('./constants');
 const deploy = require('./deploy');
 const utils = require('./utils');
 
 module.exports.constants = constants;
 module.exports.deploy = deploy;
-
-module.exports.dashboardAdmins = middlewares.dashboardAdmins;
-module.exports.requireUser = middlewares.requireUser;
 
 module.exports.unifyDatabases = utils.unifyDatabases;
 module.exports.unifyScripts = utils.unifyScripts;
--- src/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-const dashboardAdmins = require('./dashboardAdmins');
-const requireUser = require('./requireUser');
-
-module.exports.dashboardAdmins = dashboardAdmins;
-module.exports.requireUser = requireUser;
