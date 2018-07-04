8089a49c8965cf894bee8a4c1df5654aff1b8abc
Steve Adams <stevezau@gmail.com>
2018-07-04 23:27:08 +1000

    refactor/cleanup utils

--- src/utils.js
+++ src/utils.js
@@ -1,56 +1,51 @@
-const _ = require('lodash');
-const crypto = require('crypto');
-const ValidationError = require('auth0-extension-tools').ValidationError;
-const ArgumentError = require('auth0-extension-tools').ArgumentError;
+import crypto from 'crypto';
+import { ValidationError, ArgumentError } from 'auth0-extension-tools';
 
-const keywordReplace = function(input, mappings) {
+export function keywordReplace(input, mappings) {
   if (mappings && Object.keys(mappings).length > 0) {
     Object.keys(mappings).forEach(function(key) {
-      const re = new RegExp('##' + key + '##', 'g');
+      const re = new RegExp(`##${key}##`, 'g');
       input = input.replace(re, mappings[key]);
     });
 
     Object.keys(mappings).forEach(function(key) {
-      const re = new RegExp('@@' + key + '@@', 'g');
+      const re = new RegExp(`@@${key}@@`, 'g');
       input = input.replace(re, JSON.stringify(mappings[key]));
     });
   }
   return input;
-};
+}
 
-const unifyScripts = function(data, mappings) {
-  const converted = {};
-  _.forEach(data, function(item) {
-    _.keys(item)
-      .filter(function(key) {
-        return key.endsWith('File');
-      })
-      .forEach(function(key) {
-        /* foreach attribute that ends in file, do a keyword replacement, or stringify it */
+export function unifyScripts(data, mappings) {
+  /* foreach attribute that ends in file, do a keyword replacement, or stringify it */
+  data.forEach((item) => {
+    Object.keys(item).forEach((key) => {
+      if (key.endsWith('File')) {
         if (typeof item[key] === 'object') {
           item[key] = JSON.stringify(item[key]);
         } else if (item[key]) {
           item[key] = keywordReplace(item[key], mappings);
         }
-      });
-
-    converted[item.name] = item;
+      }
+    });
   });
 
-  return converted;
-};
+  return data.reduce((accum, item) => {
+    accum[item.name] = item;
+    return accum;
+  }, {});
+}
 
-const generateChecksum = function(data) {
+export function generateChecksum(data) {
   if (typeof data !== 'string') {
     throw new ArgumentError('Must provide data as a string.');
   }
 
-  const checksum = crypto.createHash('sha256').update(data).digest('hex');
-  return checksum;
-};
+  return crypto.createHash('sha256').update(data).digest('hex');
+}
 
-module.exports.parseJsonFile = function(fileName, contents, mappings) {
-  var json = contents;
+export function parseJsonFile(fileName, contents, mappings) {
+  let json = contents;
   try {
     /* if mappings is defined, replace contents before parsing */
     json = keywordReplace(contents, mappings);
@@ -58,9 +53,9 @@ module.exports.parseJsonFile = function(fileName, contents, mappings) {
   } catch (e) {
     throw new ValidationError('Error parsing JSON from metadata file: ' + fileName + ', because: ' + e.message + ', contents: ' + contents + ', post-replace: ' + json);
   }
-};
+}
 
-module.exports.checksumReplacer = function(exclusions) {
+export function checksumReplacer(exclusions) {
   exclusions = exclusions || [];
   if (typeof exclusions === 'string') {
     exclusions = [ exclusions ];
@@ -68,26 +63,16 @@ module.exports.checksumReplacer = function(exclusions) {
 
   return function(key, value) {
     if (exclusions.indexOf(key) > -1 && typeof value === 'string') {
-      const checksum = generateChecksum(value);
-      return checksum;
+      return generateChecksum(value);
     }
 
     return value;
   };
-};
+}
 
-module.exports.unifyDatabases = function(data, mappings) {
-  const converted = [];
-  _.forEach(data, function(item) {
-    const connection = {
-      name: item.name,
-      scripts: unifyScripts(item.scripts, mappings)
-    };
-
-    converted.push(connection);
-  });
-
-  return converted;
-};
-
-module.exports.unifyScripts = unifyScripts;
+export function unifyDatabases(data, mappings) {
+  return data.map(item => ({
+    name: item.name,
+    scripts: unifyScripts(item.scripts, mappings)
+  }));
+}
