697dabd9d8fc333fa83db070cef7fa1f80436d6e
Steve Adams <stevezau@gmail.com>
2018-07-08 20:22:07 +1000

    lint errors

--- src/auth0/handlers/pages.js
+++ src/auth0/handlers/pages.js
@@ -2,8 +2,9 @@ import DefaultHandler from './default';
 import constants from '../../constants';
 import { dumpJSON } from '../../utils';
 
-const supportedPages = constants.PAGE_NAMES.filter(p =>
-  p.includes('.json')).map(p => p.replace('.json', ''));
+const supportedPages = constants.PAGE_NAMES
+  .filter(p => p.includes('.json'))
+  .map(p => p.replace('.json', ''));
 
 export const schema = {
   type: 'array',
