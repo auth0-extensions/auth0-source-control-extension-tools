b9d7f43d12a0c78b283d68cc3642a0e12675d0e2
Steve Adams <stevezau@gmail.com>
2018-07-08 20:21:36 +1000

    add pages support

--- src/auth0/handlers/pages.js
+++ src/auth0/handlers/pages.js
@@ -1,7 +1,9 @@
 import DefaultHandler from './default';
 import constants from '../../constants';
+import { dumpJSON } from '../../utils';
 
-const supportedPages = constants.PAGE_NAMES.filter(p => p.includes('.json')).map(p => p.replace('.json', ''));
+const supportedPages = constants.PAGE_NAMES.filter(p =>
+  p.includes('.json')).map(p => p.replace('.json', ''));
 
 export const schema = {
   type: 'array',
@@ -10,12 +12,17 @@ export const schema = {
     properties: {
       name: { type: 'string', enum: supportedPages },
       html: { type: 'string', default: '' },
-      enabled: { type: 'boolean', default: true }
+      enabled: { type: 'boolean' }
     },
     require: [ 'html', 'name' ]
   }
 };
 
+const pageNameMap = {
+  guardian_multifactor: 'guardian_mfa_page',
+  password_reset: 'change_password',
+  error_page: 'error_page'
+};
 
 export default class PageHandler extends DefaultHandler {
   constructor(options) {
@@ -25,10 +32,67 @@ export default class PageHandler extends DefaultHandler {
     });
   }
 
+  didUpdate(page) {
+    const dump = { ...page };
+    delete dump.html;
+    this.log(`Updated [${this.type}]: ${dumpJSON(dump)}`);
+  }
+
+  async updateLoginPage(page) {
+    const globalClient = await this.client.clients.getAll({ is_global: true });
+
+    if (!globalClient[0]) {
+      throw new Error('Unable to find global client id when trying to update the login page');
+    }
+
+    await this.client.clients.update(
+      { client_id: globalClient[0].client_id },
+      {
+        custom_login_page: page.html,
+        custom_login_page_on: page.enabled
+      }
+    );
+    this.updated += 1;
+    this.didUpdate(page);
+  }
+
+  async updatePages(pages) {
+    const toUpdate = pages.filter(p => supportedPages.includes(p.name));
+    const update = toUpdate.reduce((accum, page) => {
+      if (supportedPages.includes(page.name)) {
+        const pageName = pageNameMap[page.name];
+        if (!pageName) {
+          throw new Error(`Unable to map page ${page.name} into tenant level page setting`);
+        }
+        accum[pageName] = { ...page };
+        delete accum[pageName].name;
+      }
+      return accum;
+    }, {});
+
+    await this.client.tenant.updateSettings(update);
+
+    toUpdate.forEach((page) => {
+      this.updated += 1;
+      this.didUpdate(page);
+    });
+  }
+
   async getType() {
+    // Override and return nothing as we don't need to get pages, just update them.
     return [];
   }
 
-  async processChanges() {
+  async processChanges(assets) {
+    const { pages } = assets;
+
+    // Login page is handled via the global client
+    const loginPage = pages.filter(p => p.name === 'login')[0];
+    if (loginPage) {
+      await this.updateLoginPage(loginPage);
+    }
+
+    // Rest of pages are on tenant level settings
+    await this.updatePages(pages.filter(p => p.name !== 'login'));
   }
 }
