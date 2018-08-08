6190d0fc9449522986d1d155a998086575b963e2
Steve Adams <stevezau@gmail.com>
2018-08-08 21:48:08 +1000

    won't delete clients unless told.

--- src/auth0/handlers/clients.js
+++ src/auth0/handlers/clients.js
@@ -1,4 +1,5 @@
 import DefaultHandler from './default';
+import { dumpJSON } from '../../utils';
 
 export const schema = {
   type: 'array',
@@ -38,6 +39,23 @@ export default class ClientHandler extends DefaultHandler {
     return super.didUpdate({ name: client.name, client_id: client.client_id });
   }
 
+  async processChanges(assets) {
+    const changes = await this.calcChanges(assets);
+
+    // Don't delete clients unless told as you cannot recover client_id's if deleted.
+    const shouldDelete = this.config('AUTH0_ALLOW_CLIENT_DELETE') === 'true' || this.config('AUTH0_ALLOW_CLIENT_DELETE') === true;
+    if (!shouldDelete) {
+      if (changes.del.length > 0) {
+        this.log(`WARNING: Detected the following clients should be deleted.
+        Doing so will be prevent authentications using the client_id. You can force deletes by setting 'AUTH0_ALLOW_CLIENT_DELETE' to true in the config
+        \n${dumpJSON(changes.del.map(client => ({ name: client.name, client_id: client.client_id })), 2)})
+         `);
+      }
+      changes.del = [];
+    }
+
+    return super.processChanges(assets, { ...changes });
+  }
 
   async getType() {
     if (this.existing) {
