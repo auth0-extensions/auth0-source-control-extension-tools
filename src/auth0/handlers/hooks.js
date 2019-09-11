a1e7c01425565ad774cb2d3b951eabdb51b3af18
Alexander Zarubin <nonexist.void@gmail.com>
2019-09-11 16:28:22 +0300

    hooks management + tests

--- src/auth0/handlers/hooks.js
+++ src/auth0/handlers/hooks.js
@@ -1,5 +1,6 @@
-import { calcChanges } from '../../utils';
 import DefaultHandler from './default';
+
+const ALLOWED_TRIGGER_IDS = [ 'credentials-exchange', 'pre-user-registration', 'post-user-registration' ];
 
 export const excludeSchema = {
   type: 'array',
@@ -17,11 +18,6 @@ export const schema = {
         description: 'A script that contains the hook\'s code',
         default: ''
       },
-      id: {
-        type: 'string',
-        description: 'The ulid of the hook.',
-        pattern: '^[^-\\s][a-zA-Z0-9-\\s]+[^-\\s]$'
-      },
       name: {
         type: 'string',
         description: 'The name of the hook. Can only contain alphanumeric characters, spaces and \'-\'. Can neither start nor end with \'-\' or spaces',
@@ -30,13 +26,12 @@ export const schema = {
       active: {
         type: 'boolean',
         description: 'true if the hook is active, false otherwise',
-        default: true
+        default: false
       },
       triggerId: {
         type: 'string',
         description: 'The hooks\'s trigger ID',
-        default: 'credentials-exchange',
-        enum: [ 'credentials-exchange', 'pre-user-registration', 'post-user-registration' ]
+        enum: ALLOWED_TRIGGER_IDS
       },
       secrets: {
         type: 'array',
@@ -50,17 +45,26 @@ export const schema = {
         description: 'List of key-value pairs of NPM dependencies available to the hook.'
       }
     },
-    required: [ 'code', 'name', 'active', 'triggerId' ]
+    required: [ 'code', 'name', 'triggerId' ]
   }
 };
 
+const getActive = (hooks) => {
+  const result = {};
+
+  ALLOWED_TRIGGER_IDS.forEach((type) => {
+    result[type] = hooks.filter(h => h.active && h.triggerId === type);
+  });
+
+  return result;
+};
 
 export default class HooksHandler extends DefaultHandler {
   constructor(options) {
     super({
       ...options,
       type: 'hooks',
-      stripUpdateFields: [ 'triggerId' ] // Fields not allowed in updates
+      stripUpdateFields: [ 'id' ] // TODO: decide if we want to restrict changing triggerId
     });
   }
 
@@ -68,19 +72,52 @@ export default class HooksHandler extends DefaultHandler {
     return super.objString({ name: hook.name, triggerId: hook.triggerId });
   }
 
-  async calcChanges(assets) {
-    const { hooks } = assets;
+  async getType() {
+    if (this.existing) {
+      return this.existing;
+    }
 
-    // Figure out what needs to be updated vs created
-    // const {
-    //   del, update, create, conflicts
-    // } = calcChanges(hooks, [], [ 'triggerId', 'name' ]);
+    // in case client version does not support hooks
+    if (!this.client.hooks || typeof this.client.hooks.getAll !== 'function') {
+      return {};
+    }
+
+    this.existing = await this.client.hooks.getAll();
+    return this.existing;
+  }
+
+  async calcChanges(assets) {
     const {
       del, update, create, conflicts
-    } = calcChanges(hooks, [], [ 'id' ]);
+    } = await super.calcChanges(assets);
+
+    // If ALLOW_DELETE is set to TRUE, the app will remove all hooks that are not in the assets
+    if (this.config('AUTH0_ALLOW_DELETE') === 'true' || this.config('AUTH0_ALLOW_DELETE') === true) {
+      return {
+        del,
+        update,
+        create,
+        conflicts
+      };
+    }
+
+    // Otherwise we have to make sure that existing hooks will be deactivated
+    const active = getActive([ ...create, ...update, ...conflicts ]);
+    const filtered = del.filter((hook) => {
+      const activeOfType = active[hook.triggerId] && active[hook.triggerId][0] && active[hook.triggerId][0].name;
+
+      // deactivating the existing hook only if we have another active hook in the same category
+      if (hook.active && activeOfType && hook.name !== activeOfType) {
+        hook.active = false;
+        update.push(hook);
+        return false;
+      }
+
+      return true;
+    });
 
     return {
-      del,
+      del: filtered,
       update,
       create,
       conflicts
@@ -93,7 +130,16 @@ export default class HooksHandler extends DefaultHandler {
     // Do nothing if not set
     if (!hooks) return;
 
-    // TODO: Implement when adding CRUD
+    const activeHooks = getActive(hooks);
+
+    ALLOWED_TRIGGER_IDS.forEach((type) => {
+      if (activeHooks[type].length > 1) { // There can be only one!
+        const conflict = activeHooks[type].map(h => h.name).join(', ');
+        const err = new Error(`Only one active hook allowed for "${type}" extensibility point. Conflicting hooks: ${conflict}`);
+        err.status = 409;
+        throw err;
+      }
+    });
 
     await super.validate(assets);
   }
@@ -106,7 +152,6 @@ export default class HooksHandler extends DefaultHandler {
 
     // Figure out what needs to be updated vs created
     const changes = await this.calcChanges(assets);
-
     await super.processChanges(assets, {
       del: changes.del,
       create: changes.create,
