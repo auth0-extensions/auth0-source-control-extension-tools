80f1ca79f08d1ceb6aa8a605c5e3066e576daa23
Cristofer Gonzales <cristofer_g@hotmail.com>
2020-06-30 16:09:04 -0400

    Add migration logic to warn on unavailable migrations

--- src/auth0/handlers/migrations.js
+++ src/auth0/handlers/migrations.js
@@ -1,4 +1,5 @@
 import DefaultHandler, { order } from './default';
+import log from '../../logger';
 
 export const schema = {
   type: 'object',
@@ -23,8 +24,22 @@ export default class MigrationsHandler extends DefaultHandler {
   @order('150')
   async processChanges(assets) {
     const { migrations } = assets;
+    const ignoreUnavailableMigrations = this.config('AUTH0_IGNORE_UNAVAILABLE_MIGRATIONS');
 
     if (migrations && Object.keys(migrations).length > 0) {
+      const existingMigrations = await this.client.migrations.getMigrations();
+      const supportedMigrations = Object.keys(existingMigrations.flags);
+      const unavailableMigrations = Object.keys(migrations).filter(flag => !supportedMigrations.includes(flag));
+      const unavailableDisabledMigrations = unavailableMigrations.filter(flag => migrations[flag] === false);
+
+      if (ignoreUnavailableMigrations && unavailableMigrations.length > 0) {
+        log.info(`The following migrations are not available '${unavailableDisabledMigrations.join(',')}'. The migrations will be ignored because you have AUTH0_IGNORE_UNAVAILABLE_MIGRATIONS=true in your configuration.`);
+        unavailableMigrations.forEach(flag => delete migrations[flag]);
+      } else if (unavailableDisabledMigrations.length > 0) {
+        log.warn(`The following disabled migrations are not available '${unavailableDisabledMigrations.join(',')}'. The migrations will be ignored, remove the migrations to avoid future warnings.`);
+        unavailableDisabledMigrations.forEach(flag => delete migrations[flag]);
+      }
+
       await this.client.migrations.updateMigrations({ flags: migrations });
       this.updated += 1;
       this.didUpdate(migrations);
--- tests/auth0/handlers/migrations.tests.js
+++ tests/auth0/handlers/migrations.tests.js
@@ -2,44 +2,96 @@ const { expect } = require('chai');
 const migrations = require('../../../src/auth0/handlers/migrations');
 
 describe('#migrations handler', () => {
-  describe('#migrations process', () => {
-    it('should get migration flags', async () => {
-      const auth0 = {
-        migrations: {
-          getMigrations: () => ({
-            flags: {
-              migration_flag: true
-            }
-          })
+  const mockClient = flags => ({
+    migrations: {
+      getMigrations: () => ({
+        flags: {
+          migration_flag: true
         }
-      };
+      }),
+      updateMigrations: (data) => {
+        expect(data).to.be.an('object');
+        expect(data).to.have.deep.property('flags', { migration_flag: false, ...flags });
+        return Promise.resolve(data);
+      }
+    }
+  });
 
-      const handler = new migrations.default({ client: auth0 });
+  describe('#getType()', () => {
+    it('should get migration flags', async () => {
+      const client = mockClient();
+
+      const handler = new migrations.default({ client });
       const data = await handler.getType();
       expect(data).to.deep.equal({
         migration_flag: true
       });
     });
+  });
 
-    it('should update migration flags', async () => {
-      const auth0 = {
-        migrations: {
-          updateMigrations: (data) => {
-            expect(data).to.be.an('object');
-            expect(data).to.have.deep.property('flags', { new_migration_flag: true });
-            return Promise.resolve(data);
-          }
-        }
-      };
+  describe('#migrations process', () => {
+    it('should update available migration flags', async () => {
+      const client = mockClient();
+      const config = () => false;
 
-      const handler = new migrations.default({ client: auth0 });
+      const handler = new migrations.default({ client, config });
       const stageFn = Object.getPrototypeOf(handler).processChanges;
 
       await stageFn.apply(handler, [ {
         migrations: {
-          new_migration_flag: true
+          migration_flag: false
         }
       } ]);
+    });
+
+    describe('when AUTH0_IGNORE_UNAVAILABLE_MIGRATIONS=false (default)', () => {
+      it('should ignore unavailable disabled migration flags', async () => {
+        const client = mockClient();
+        const config = () => false;
+
+        const handler = new migrations.default({ client, config });
+        const stageFn = Object.getPrototypeOf(handler).processChanges;
+
+        await stageFn.apply(handler, [ {
+          migrations: {
+            migration_flag: false,
+            disabled_flag: false
+          }
+        } ]);
+      });
+
+      it('should not ignore unavailable enabled migration flags', async () => {
+        const client = mockClient({ disabled_flag: true });
+        const config = () => false;
+
+        const handler = new migrations.default({ client, config });
+        const stageFn = Object.getPrototypeOf(handler).processChanges;
+
+        await stageFn.apply(handler, [ {
+          migrations: {
+            migration_flag: false,
+            disabled_flag: true
+          }
+        } ]);
+      });
+    });
+
+    describe('when AUTH0_IGNORE_UNAVAILABLE_MIGRATIONS=true', () => {
+      it('should ignore all unavailable migration flags', async () => {
+        const client = mockClient();
+        const config = name => name === 'AUTH0_IGNORE_UNAVAILABLE_MIGRATIONS';
+
+        const handler = new migrations.default({ client, config });
+        const stageFn = Object.getPrototypeOf(handler).processChanges;
+
+        await stageFn.apply(handler, [ {
+          migrations: {
+            migration_flag: false,
+            disabled_flag: true,
+            another_disabled_flag: false
+          }
+        } ]);
+      });
     });
   });
 });
