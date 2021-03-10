f4a350002c52255a9c1a1eb9838c55952e8a9cbb
Danny <danny.turcotte@auth0.com>
2021-03-10 15:49:22 -0500

    Fix: webauthn platform (#132)
    * fix: add webauthn platform as a supported factor (#128)
    
    * 4.4.2
    
    Co-authored-by: Jarrett Helton <jaydhelton@gmail.com>

--- src/constants.js
+++ src/constants.js
@@ -113,7 +113,8 @@ constants.GUARDIAN_FACTORS = [
   'otp',
   'email',
   'duo',
-  'webauthn-roaming'
+  'webauthn-roaming',
+  'webauthn-platform'
 ];
 constants.GUARDIAN_POLICIES = [
   'all-applications',
